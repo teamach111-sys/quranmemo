cd771a99e4aa9330c1bb130405f4b428d8217f7d
mohamed <teamach@gmail.com>
2026-06-26 20:37:23 +0100

    done favicon and other logos

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -4,14 +4,14 @@
 
 @if($sidebar)
     <flux:sidebar.brand  {{ $attributes }}>
-        <x-slot name="logo" class="flex w-full items-center justify-center ">
-            <x-app-logo-icon class="size-20 fill-current text-white dark:text-black" />
+        <x-slot name="logo" class=" w-full h-full items-center justify-center ">
+            <x-app-logo-icon class=" text-white dark:text-black" />
         </x-slot>
     </flux:sidebar.brand>
 @else
     <flux:sidebar.brand  {{ $attributes }}>
-        <x-slot name="logo" class="flex w-full items-center justify-center ">
-            <x-app-logo-icon class="size-20 fill-current text-white dark:text-black" />
+        <x-slot name="logo" class=" w-full h-full items-center justify-center ">
+            <x-app-logo-icon class=" text-white dark:text-black" />
         </x-slot>
     </flux:sidebar.brand>
 @endif
