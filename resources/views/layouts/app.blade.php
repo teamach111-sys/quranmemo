d46c5f89d7abe0d1bd825771c243263fd45fa77f
mohamed <teamach@gmail.com>
2026-06-27 20:01:47 +0100

    finished fixing global year selector responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <x-layouts::app.sidebar :title="$title ?? null">
-    <div class="h-20 bg-[#FAFAFA] w-full p-5 hidden border-b border-[#DEDEDE] lg:block">
-        <select name="" id="" class="p-2 border h-10 rounded-md w-40">
+    <div class="h-20 bg-[#FAFAFA] dark:text-white dark:bg-[#171717] dark:border-[#3E3E3E] w-full p-5 hidden border-b border-[#DEDEDE] lg:block">
+        <select name="" id="" class="p-2 border focus:outline-none border-[#DEDEDE] h-10 rounded-md w-40">
             <option class="dark:bg-white dark:text-black dark:hover:bg-slate-100 border" value="">2026/2027</option>
             <option class="dark:bg-white dark:text-black dark:hover:bg-slate-100 border" value="">2024/2025</option>
             <option class="dark:bg-white dark:text-black dark:hover:bg-slate-100 border" value="">2023/2024</option>
