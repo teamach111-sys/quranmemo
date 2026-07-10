d705fc0c2adb537820dac398a10732dca891aeac
mohamed <teamach@gmail.com>
2026-07-11 00:23:01 +0100

    fixed adding filiere modal buttons

--- resources/views/components/⚡createprogramme.blade.php
+++ resources/views/components/⚡createprogramme.blade.php
@@ -2,41 +2,36 @@
 
 use Livewire\Component;
 
-new class extends Component
-{
+new class extends Component {
     public $message = null;
     public $nom;
     public $description;
     public $nombre_annees;
 
-
-  
     #[\Livewire\Attributes\On('reset-message')]
-    public function resetMessage(){
+    public function resetMessage()
+    {
         $this->message = null;
     }
-    
 
-    public function store(){
+    public function store()
+    {
         $this->validate([
-         'nom' => 'required|string|max:50',
-         'description' => 'nullable|string',
-         'nombre_annees' => 'required|integer|min:1',
-    ]);
+            'nom' => 'required|string|max:50',
+            'description' => 'nullable|string',
+            'nombre_annees' => 'required|integer|min:1',
+        ]);
 
-    \App\Models\Programme::create([
-        'nom' => $this->nom,
-        'description' => $this->description,
-        'nombre_annees' => $this->nombre_annees,
-    ]);
+        \App\Models\Programme::create([
+            'nom' => $this->nom,
+            'description' => $this->description,
+            'nombre_annees' => $this->nombre_annees,
+        ]);
 
-    $this->reset();
-    $this->dispatch('refreshtable');
-    $this->message = 'Programme ajouté avec succès !';
+        $this->reset();
+        $this->dispatch('refreshtable');
+        $this->message = 'Programme ajouté avec succès !';
     }
-
-   
-    
 };
 ?>
 
@@ -62,31 +57,40 @@ new class extends Component
         @endif
     </div>
     <form wire:submit.prevent="store">
-    @csrf
+        @csrf
         <div class="grid grid-cols-2 w-full gap-4">
             <div>
                 <label class="block text-sm font-medium mb-1">Nom</label>
                 <input type="text" wire:model="nom" id="nom" class="rounded-md border w-full p-2"
                     placeholder="Ex: diplome de ....">
             </div>
-            
+
             <div>
                 <label class="block text-sm font-medium mb-1">Description</label>
                 <input type="text" wire:model="description" id="description" class="rounded-md border w-full p-2"
                     placeholder="Description....">
             </div>
-            
+
             <div>
                 <label class="block text-sm font-medium mb-1">Nombre d'Années</label>
                 <input type="number" wire:model="nombre_annees" id="nombre_annees" class="rounded-md border w-full p-2"
                     placeholder="Ex: 2">
             </div>
         </div>
+        <div class="flex gap-3 pt-4 w-full">
+            <button type="submit"
+                class="dark:bg-white dark:text-black dark:hover:bg-slate-100
+  flex-1 rounded-md bg-[#262626] hover:bg-[#3B3B3B] text-white px-4 py-2 cursor-pointer ">
+                Ajouter la filière
+            </button>
+              <button  type="button" @click="open = false; $wire.dispatch('reset-message')"
+                class="dark:bg-white dark:text-black dark:hover:bg-slate-100
+  flex-1 rounded-md bg-[#262626] hover:bg-[#3B3B3B] text-white px-4 py-2 cursor-pointer ">
+                Fermer
+            </button>
 
-        <button type="submit" wire:dispatch('reset-message')
-            class="mt-5 dark:bg-white dark:text-black dark:hover:bg-slate-100 flex-1 rounded-md bg-[#262626] hover:bg-[#3B3B3B] text-white px-4 py-2 cursor-pointer flex gap-2 items-center ">
-           <x-codicon-add class="h-5 w-5" /> Ajouter le Programme
-        </button>
+
+        </div>
     </form>
 
 </div>
